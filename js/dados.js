08ffbbd465b5ebf20caa10b25d61c8a5b19949ec
EstefaneFreitas <estefane.freitas.dev@gmail.com>
2024-09-07 23:37:45 -0300

    primeiro commit

---
+++ js/dados.js
@@ -0,0 +1,39 @@
+const atoresMarvel = [
+  {
+    nome: "Robert Downey Jr.",
+    idade: 58,
+    filme: "Homem de Ferro",
+    ano: 2008,
+    personagem: "Tony Stark / Homem de Ferro",
+  },
+  {
+    nome: "Scarlett Johansson",
+    idade: 38,
+    filme: "Os Vingadores",
+    ano: 2012,
+    personagem: "Natasha Romanoff / Viúva Negra",
+  },
+  {
+    nome: "Chris Hemsworth",
+    idade: 39,
+    filme: "Thor",
+    ano: 2011,
+    personagem: "Thor",
+  },
+  {
+    nome: "Brie Larson",
+    idade: 33,
+    filme: "Capitã Marvel",
+    ano: 2019,
+    personagem: "Carol Danvers / Capitã Marvel",
+  },
+  {
+    nome: "Tom Holland",
+    idade: 27,
+    filme: "Homem-Aranha: De Volta ao Lar",
+    ano: 2017,
+    personagem: "Peter Parker / Homem-Aranha",
+  },
+];
+
+console.log(atoresMarvel[0]);

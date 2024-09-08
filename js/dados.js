600ac60dca7da2df8edfec1fb673e75bf250dbde
EstefaneFreitas <estefane.freitas.dev@gmail.com>
2024-09-08 01:22:44 -0300

    Adicionando Script para mostrar o elenco

--- js/dados.js
+++ js/dados.js
@@ -1,39 +1,42 @@
-const atoresMarvel = [
+var elencos = [
   {
-    nome: "Robert Downey Jr.",
-    idade: 58,
-    filme: "Homem de Ferro",
-    ano: 2008,
-    personagem: "Tony Stark / Homem de Ferro",
+    titulo: "Robert Downey Jr. como Tony Stark/Homem de Ferro",
+    imagem: "./img/robert.webp",
+    descricaoFrase:
+      "O gênio, bilionário, playboy e filantropo que redefiniu o gênero de super-heróis.",
+    descricaoTexto:
+      " Downey Jr. trouxe à vida o carismático e sarcástico Tony Stark, um cientista brilhante que, após uma experiência traumática, se transforma no invencível Homem de Ferro. Sua performance marcante e a química com o público tornaram o personagem um dos mais icônicos do Universo Cinematográfico Marvel.",
   },
   {
-    nome: "Scarlett Johansson",
-    idade: 38,
-    filme: "Os Vingadores",
-    ano: 2012,
-    personagem: "Natasha Romanoff / Viúva Negra",
+    titulo: "Scarlett Johansson como Natasha Romanoff/Viúva Negra",
+    imagem: "./img/scarlet.webp",
+    descricaoFrase:
+      "A espiã mais letal e sedutora do mundo, com um coração de ouro escondido sob camadas de mistério.",
+    descricaoTexto:
+      "Johansson interpretou a enigmática Viúva Negra com maestria,combinando habilidades de combate impressionantes com uma vulnerabilidade que conquistou o público. Sua jornada desde uma assassina treinada até uma heroína que busca redenção é uma das mais comoventes da saga.",
   },
   {
-    nome: "Chris Hemsworth",
-    idade: 39,
-    filme: "Thor",
-    ano: 2011,
-    personagem: "Thor",
+    titulo: "Chris Hemsworth como Thor",
+    imagem: "./img/chris.webp",
+    descricaoFrase:
+      "O Deus do Trovão, um guerreiro poderoso e nobre, que aprendeu a humildade e a importância da família.",
+    descricaoTexto:
+      "Hemsworth encarnou o papel de Thor com força e carisma, transformando o deus nórdico em um dos personagens mais populares da Marvel. Sua evolução ao longo dos filmes, passando de um príncipe arrogante a um líder sábio e protetor, é uma das mais bem construídas do MCU.",
   },
   {
-    nome: "Brie Larson",
-    idade: 33,
-    filme: "Capitã Marvel",
-    ano: 2019,
-    personagem: "Carol Danvers / Capitã Marvel",
+    titulo: "Brie Larson como Carol Danvers/Capitã Marvel",
+    imagem: "./img/brie.webp",
+    descricaoFrase:
+      "A heroína mais poderosa do universo, que inspirou uma geração com sua força e determinação.",
+    descricaoTexto:
+      "Larson trouxe à vida a Capitã Marvel com uma intensidade e presença de tela que a tornaram um ícone instantâneo. Sua jornada de autodescoberta e a luta por seus poderes a transformaram em um símbolo de empoderamento feminino.",
   },
   {
-    nome: "Tom Holland",
-    idade: 27,
-    filme: "Homem-Aranha: De Volta ao Lar",
-    ano: 2017,
-    personagem: "Peter Parker / Homem-Aranha",
+    titulo: "Tom Holland como Peter Parker/Homem-Aranha",
+    imagem: "./img/tom.jpg",
+    descricaoFrase:
+      "O amigável vizinho da Marvel, um adolescente com grandes responsabilidades e um coração ainda maior.",
+    descricaoTexto:
+      "  Holland trouxe uma nova energia e frescor ao papel do Homem-Aranha, capturando perfeitamente a inocência e o entusiasmo do personagem. Sua química com os outros heróis e sua jornada como um jovem tentando conciliar sua vida pessoal com seus deveres como super-herói cativaram o público de todas as idades.",
   },
 ];
-
-console.log(atoresMarvel[0]);
